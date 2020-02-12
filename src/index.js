9f5157aa8b020dfb9c4ef3c0762c82706a2a6121
Egor <Twinkstriker@gmail.com>
2020-02-12 23:47:17 +0300

    for gh-pages

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import Main from './app.jsx';
+
+import './index.scss';
+
+ReactDOM.render(<Main/>, document.querySelector('#root'));
