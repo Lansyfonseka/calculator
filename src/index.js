f0c4a95d9da6ea30207a3625a65caf2d9cfca778
Egor <Twinkstriker@gmail.com>
2020-02-12 23:34:23 +0300

    clean brang

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
-import Main from './app.jsx';
-
-import './index.scss';
-
-ReactDOM.render(<Main/>, document.querySelector('#root'));
